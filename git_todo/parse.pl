fcd57ec7d1b788d6d2e3a2e921a20ccef2209656
Struan Donald <struan@spooky.exo.org.uk>
2009-02-02 21:02:26 +0000

    parse that works with multiple todo levels ok

--- git_todo/parse.pl
+++ git_todo/parse.pl
@@ -4,22 +4,44 @@ use strict;
 use warnings;
 use 5.010;
 
-my ( $category, $prev_line, $indent_level, %msg );
+my ( $category, $prev_line, @indent_titles, %msg, %titles_out );
 
 while ( <> ) {
     chomp;
     if ( /^[A-Z0-9 ]+$/ ) {
         $category = $_;
+        @indent_titles = ();
         next;
     }
 
+    if ( $prev_line and /^\s+/ ) {
+        my ( $indent ) = m/^(\s+)\S/;
+        my ( $prev_indent ) = ( $prev_line =~ m/^(\s+)\S/ );
+
+        my $d = length( $indent ) - length( $prev_indent );
+
+        if ( $d > 0 ) {
+            push @indent_titles, $prev_line;
+        } elsif ( $d < 0 ) {
+            pop @indent_titles;
+        }
+    }
+
     if ( /^\s+X/ ) {
+        my $title = $indent_titles[ -1 ] if ( @indent_titles );
+        
         unless( exists ( $msg{ $category } ) ) {
            $msg{ $category } = []; 
         }
 
+        if ( $title and not exists $titles_out{ join( ':', @indent_titles ) } ) {
+            push( @{ $msg{ $category } }, $title );
+            $titles_out{ join( ':', @indent_titles ) }++;
+        }
         push( @{ $msg{ $category } }, $_ );
     }
+
+    $prev_line = $_;
 }
 
 for my $cat ( keys( %msg ) ) {
