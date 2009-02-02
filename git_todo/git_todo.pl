190b6dfda6da9827b05e69812df0ba50f1fd6d61
Struan Donald <struan@spooky.exo.org.uk>
2009-02-02 21:06:22 +0000

    munged in improved parse loop

--- git_todo/git_todo.pl
+++ git_todo/git_todo.pl
@@ -5,28 +5,48 @@ use Git;
 
 chdir '/c/Documents and Settings/struan.donald/My Documents/docs/git/' ;
 
-my ( $category, $prev_line, $indent_level, %msg, $todo );
+my ( $category, $prev_line, @indent_titles, %msg, %titles_out, $todo );
 
 $todo = '';
 
 open TODO, 'TODO.txt' or die "Failed to open TODO.txt: $!\n";
 while ( <TODO> ) {
     chomp;
-    if ( /^[A-Z0-9][A-Z0-9 ]+/ ) {
+    if ( /^[A-Z0-9 ]+$/ ) {
         $category = $_;
-        $todo .= "$_\n";
+        @indent_titles = ();
         next;
     }
 
+    if ( $prev_line and /^\s+/ ) {
+        my ( $indent ) = m/^(\s+)\S/;
+        my ( $prev_indent ) = ( $prev_line =~ m/^(\s+)\S/ );
+
+        my $d = length( $indent ) - length( $prev_indent );
+
+        if ( $d > 0 ) {
+            push @indent_titles, $prev_line;
+        } elsif ( $d < 0 ) {
+            pop @indent_titles;
+        }
+    }
+
     if ( /^\s+X/ ) {
+        my $title = $indent_titles[ -1 ] if ( @indent_titles );
+        
         unless( exists ( $msg{ $category } ) ) {
-           $msg{ $category } = [];
+           $msg{ $category } = []; 
         }
 
+        if ( $title and not exists $titles_out{ join( ':', @indent_titles ) } ) {
+            push( @{ $msg{ $category } }, $title );
+            $titles_out{ join( ':', @indent_titles ) }++;
+        }
         push( @{ $msg{ $category } }, $_ );
-    } else {
-        $todo .= "$_\n";
     }
+
+    $prev_line = $_;
+
 }
 close TODO;
 
